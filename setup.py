e9770516f53184c3caa1a488651efa1147424f6d
John T. Wodder II <git@varonathe.org>
2017-02-06 18:40:34 +0000

    Run pytest directly instead of through setup.py

--- setup.py
+++ setup.py
@@ -33,9 +33,7 @@ setup(
     long_description=long_desc,
     url='https://github.com/jwodder/headerparser',
 
-    setup_requires=['pytest-runner~=2.0'],
     install_requires=['six~=1.1'],
-    tests_require=['pytest~=3.0', 'pytest-cov~=2.0', 'pytest-flakes~=1.0'],
 
     classifiers=[
         'Development Status :: 3 - Alpha',
