1dc911b63ae8786f776cce5cccfa61e2d1870346
John T. Wodder II <git@varonathe.org>
2019-05-22 20:49:20 -0400

    Run scanner tests against both scanner functions

--- test/test_scanner.py
+++ test/test_scanner.py
@@ -1,29 +1,40 @@
 import re
 import pytest
+from   six          import StringIO
 import headerparser
 from   headerparser import scan, scan_file, scan_lines, scan_string
 
-def test_simple():
-    assert list(scan_string('Foo: red\nBar: green\nBaz: blue\n')) == \
+def scan_string_as_file(s, **kwargs):
+    return scan(StringIO(s), **kwargs)
+
+def scan_string_as_list(s, **kwargs):
+    return scan(s.splitlines(True), **kwargs)
+
+@pytest.fixture(params=[scan_string_as_file, scan_string_as_list, scan_string])
+def scanner(request):
+    return request.param
+
+def test_simple(scanner):
+    assert list(scanner('Foo: red\nBar: green\nBaz: blue\n')) == \
         [('Foo', 'red'), ('Bar', 'green'), ('Baz', 'blue')]
 
 @pytest.mark.parametrize('skip_leading_newlines', [True, False])
-def test_empty_body(skip_leading_newlines):
-    assert list(scan_string(
+def test_empty_body(scanner, skip_leading_newlines):
+    assert list(scanner(
         'Foo: red\nBar: green\nBaz: blue\n\n',
         skip_leading_newlines=skip_leading_newlines,
     )) == [('Foo', 'red'), ('Bar', 'green'), ('Baz', 'blue'), (None, '')]
 
 @pytest.mark.parametrize('skip_leading_newlines', [True, False])
-def test_blank_body(skip_leading_newlines):
-    assert list(scan_string(
+def test_blank_body(scanner, skip_leading_newlines):
+    assert list(scanner(
         'Foo: red\nBar: green\nBaz: blue\n\n\n',
         skip_leading_newlines=skip_leading_newlines,
     )) == [('Foo', 'red'), ('Bar', 'green'), ('Baz', 'blue'), (None, '\n')]
 
 @pytest.mark.parametrize('skip_leading_newlines', [True, False])
-def test_body(skip_leading_newlines):
-    assert list(scan_string(
+def test_body(scanner, skip_leading_newlines):
+    assert list(scanner(
         'Foo: red\n'
         'Bar: green\n'
         'Baz: blue\n'
@@ -34,8 +45,8 @@ def test_body(skip_leading_newlines):
            (None, 'This is a test.')]
 
 @pytest.mark.parametrize('skip_leading_newlines', [True, False])
-def test_body_extra_blanks(skip_leading_newlines):
-    assert list(scan_string(
+def test_body_extra_blanks(scanner, skip_leading_newlines):
+    assert list(scanner(
         'Foo: red\n'
         'Bar: green\n'
         'Baz: blue\n'
@@ -46,8 +57,8 @@ def test_body_extra_blanks(skip_leading_newlines):
     )) == [('Foo', 'red'), ('Bar', 'green'), ('Baz', 'blue'),
            (None, '\nThis is a test.')]
 
-def test_headerlike_body():
-    assert list(scan_string(
+def test_headerlike_body(scanner):
+    assert list(scanner(
         'Foo: red\n'
         'Bar: green\n'
         'Baz: blue\n'
@@ -58,8 +69,8 @@ def test_headerlike_body():
     )) == [('Foo', 'red'), ('Bar', 'green'), ('Baz', 'blue'),
            (None, 'Foo: quux\nBar: glarch\nBaz: cleesh\n')]
 
-def test_circumcolon_whitespace():
-    assert list(scan_string(
+def test_circumcolon_whitespace(scanner):
+    assert list(scanner(
         'Key1: Value1\n'
         'Key2 :Value2\n'
         'Key3 : Value3\n'
@@ -67,8 +78,8 @@ def test_circumcolon_whitespace():
     )) == [('Key1', 'Value1'), ('Key2', 'Value2'), ('Key3', 'Value3'),
           ('Key4', 'Value4')]
 
-def test_circumcolon_whitespace_spaceless_separator_regex():
-    assert list(scan_string(
+def test_circumcolon_whitespace_spaceless_separator_regex(scanner):
+    assert list(scanner(
         'Key1: Value1\n'
         'Key2 :Value2\n'
         'Key3 : Value3\n'
@@ -77,8 +88,8 @@ def test_circumcolon_whitespace_spaceless_separator_regex():
     )) == [('Key1', ' Value1'), ('Key2 ', 'Value2'), ('Key3 ', ' Value3'),
           ('Key4', 'Value4')]
 
-def test_folding():
-    assert list(scan_string(
+def test_folding(scanner):
+    assert list(scanner(
         'Key1: Value1\n'
         '  Folded\n'
         '    More folds\n'
@@ -101,16 +112,16 @@ def test_folding():
         ('Key5', '\n After a line without even a space!'),
     ]
 
-def test_no_final_newline():
-    assert list(scan_string('Foo: red\nBar: green\nBaz: blue')) == \
+def test_no_final_newline(scanner):
+    assert list(scanner('Foo: red\nBar: green\nBaz: blue')) == \
         [('Foo', 'red'), ('Bar', 'green'), ('Baz', 'blue')]
 
-def test_leading_newline():
-    assert list(scan_string('\nFoo: red\nBar: green\nBaz: blue\n')) == \
+def test_leading_newline(scanner):
+    assert list(scanner('\nFoo: red\nBar: green\nBaz: blue\n')) == \
         [(None, 'Foo: red\nBar: green\nBaz: blue\n')]
 
-def test_skip_leading_newlines():
-    assert list(scan_string(
+def test_skip_leading_newlines(scanner):
+    assert list(scanner(
         '\nFoo: red\nBar: green\nBaz: blue\n',
         skip_leading_newlines=True,
     )) == [('Foo', 'red'), ('Bar', 'green'), ('Baz', 'blue')]
@@ -119,8 +130,8 @@ def test_cr_terminated():
     assert list(scan_string('Foo: red\rBar: green\rBaz: blue\r')) == \
         [('Foo', 'red'), ('Bar', 'green'), ('Baz', 'blue')]
 
-def test_crlf_terminated():
-    assert list(scan_string('Foo: red\r\nBar: green\r\nBaz: blue\r\n')) == \
+def test_crlf_terminated(scanner):
+    assert list(scanner('Foo: red\r\nBar: green\r\nBaz: blue\r\n')) == \
         [('Foo', 'red'), ('Bar', 'green'), ('Baz', 'blue')]
 
 def test_mixed_terminators():
@@ -141,22 +152,22 @@ def test_mixed_folding():
         ('Baz', 'CR\n  LF'),
     ]
 
-def test_malformed_header():
+def test_malformed_header(scanner):
     with pytest.raises(headerparser.MalformedHeaderError) as excinfo:
-        list(scan_string('Foo: red\nBar green\nBaz: blue\n'))
+        list(scanner('Foo: red\nBar green\nBaz: blue\n'))
     assert str(excinfo.value) == "Invalid header line encountered: 'Bar green'"
     assert excinfo.value.line == 'Bar green'
 
-def test_unexpected_folding():
+def test_unexpected_folding(scanner):
     with pytest.raises(headerparser.UnexpectedFoldingError) as excinfo:
-        list(scan_string(' Foo: red\nBar green\nBaz: blue\n'))
+        list(scanner(' Foo: red\nBar green\nBaz: blue\n'))
     assert str(excinfo.value) == (
         "Indented line without preceding header line encountered: ' Foo: red'"
     )
     assert excinfo.value.line == ' Foo: red'
 
-def test_multiple():
-    assert list(scan_string(
+def test_multiple(scanner):
+    assert list(scanner(
         'Foo: value1\n'
         'Foo: value2\n'
         'FOO: VALUE3\n'
@@ -169,21 +180,20 @@ def test_multiple():
     ]
 
 @pytest.mark.parametrize('skip_leading_newlines', [True, False])
-def test_empty(skip_leading_newlines):
-    assert list(scan_string('', skip_leading_newlines=skip_leading_newlines)) \
-        == []
+def test_empty(scanner, skip_leading_newlines):
+    assert list(scanner('', skip_leading_newlines=skip_leading_newlines)) == []
 
-def test_one_empty_line():
-    assert list(scan_string('\n')) == [(None, '')]
+def test_one_empty_line(scanner):
+    assert list(scanner('\n')) == [(None, '')]
 
-def test_one_empty_line_skip_leading_newlines():
-    assert list(scan_string('\n', skip_leading_newlines=True)) == []
+def test_one_empty_line_skip_leading_newlines(scanner):
+    assert list(scanner('\n', skip_leading_newlines=True)) == []
 
-def test_two_empty_lines():
-    assert list(scan_string('\n\n')) == [(None, '\n')]
+def test_two_empty_lines(scanner):
+    assert list(scanner('\n\n')) == [(None, '\n')]
 
-def test_two_empty_lines_skip_leading_newlines():
-    assert list(scan_string('\n\n', skip_leading_newlines=True)) == []
+def test_two_empty_lines_skip_leading_newlines(scanner):
+    assert list(scanner('\n\n', skip_leading_newlines=True)) == []
 
 def test_lines_no_ends():
     assert list(scan([
@@ -201,8 +211,8 @@ def test_lines_no_ends():
         (None, "Newlines will not be added to this body.So it'll look bad."),
     ]
 
-def test_untrimmed_value():
-    assert list(scan_string(
+def test_untrimmed_value(scanner):
+    assert list(scanner(
         'Leading:   value\n'
         'Trailing: value  \n'
         'Leading-Tab:\tvalue\n'
@@ -214,38 +224,38 @@ def test_untrimmed_value():
         ('Trailing-Tab', 'value\t'),
     ]
 
-def test_space_in_name():
-    assert list(scan_string('Key Name: value')) == [('Key Name', 'value')]
+def test_space_in_name(scanner):
+    assert list(scanner('Key Name: value')) == [('Key Name', 'value')]
 
 @pytest.mark.parametrize('separator_regex', [
     r'\s*=\s*',
     re.compile(r'\s*=\s*'),
 ])
-def test_separator_regex(separator_regex):
-    assert list(scan_string(
+def test_separator_regex(scanner, separator_regex):
+    assert list(scanner(
         'Foo = red\nBar =green\nBaz= blue\n',
         separator_regex=separator_regex,
     )) == [('Foo', 'red'), ('Bar', 'green'), ('Baz', 'blue')]
 
-def test_multi_colon():
-    assert list(scan_string('Foo: red : crimson: scarlet\n')) == \
+def test_multi_colon(scanner):
+    assert list(scanner('Foo: red : crimson: scarlet\n')) == \
         [('Foo', 'red : crimson: scarlet')]
 
-def test_separator_regex_multi_match():
-    assert list(scan_string(
+def test_separator_regex_multi_match(scanner):
+    assert list(scanner(
         'Foo = red = crimson=scarlet\n',
         separator_regex=r'\s*=\s*',
     )) == [('Foo', 'red = crimson=scarlet')]
 
-def test_separator_regex_mixed_multi_match():
-    assert list(scan_string(
+def test_separator_regex_mixed_multi_match(scanner):
+    assert list(scanner(
         'Key: Value = foo\nKey = Value: foo\n',
         separator_regex=r'\s*=\s*',
     )) == [('Key: Value', 'foo'), ('Key', 'Value: foo')]
 
-def test_separator_regex_default_separator():
+def test_separator_regex_default_separator(scanner):
     with pytest.raises(headerparser.MalformedHeaderError) as excinfo:
-        list(scan_string('Foo = red\nBar: green\n', separator_regex=r'\s*=\s*'))
+        list(scanner('Foo = red\nBar: green\n', separator_regex=r'\s*=\s*'))
     assert str(excinfo.value) == "Invalid header line encountered: 'Bar: green'"
     assert excinfo.value.line == 'Bar: green'
 
