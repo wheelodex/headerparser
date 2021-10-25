a3b80838703f0bb23b9f381f6f1cdaee3dd61846
John T. Wodder II <git@varonathe.org>
2021-10-25 14:54:30 -0400

    Tests to drive development

---
+++ test/test_parsable/test_parsable.py
@@ -0,0 +1,144 @@
+from typing import Any, Dict, List, Optional, Tuple, Type
+import pytest
+from headerparser import (
+    BodyField,
+    BodyNotAllowedError,
+    DuplicateFieldError,
+    ExtraFields,
+    Field,
+    MultiExtraFields,
+    MultiField,
+    UnknownFieldError,
+    decode_bool,
+    decode_value,
+    multidict,
+    parsable,
+    parse,
+)
+
+
+@parsable
+class Simple:
+    simple: str
+    optional: Optional[str] = None
+    aliased: Optional[str] = Field(alias="alias", default=None)
+    multi: List[str] = MultiField()
+    extra: List[Tuple[str, str]] = ExtraFields()
+    body: Optional[str] = BodyField()
+
+
+@parsable
+class MultiExtra:
+    foo: int = Field(decoder=decode_value(int))
+    bar: bool = Field(decoder=decode_bool)
+    extra: Dict[str, List[str]] = MultiExtraFields(decoder=multidict)
+
+
+@parsable
+class CrissCross:
+    one: str = Field(alias="two")
+    two: str = Field(alias="one")
+
+
+@pytest.mark.parametrize(
+    "data,obj",
+    [
+        (
+            Simple,
+            "Simple: foobar\n",
+            Simple(
+                simple="foobar",
+                optional=None,
+                aliased=None,
+                multi=[],
+                extra=[],
+                body=None,
+            ),
+        ),
+        (
+            Simple,
+            "Simple: foobar\nOptional: present\nAlias: unknown\nAliased: extra\n"
+            "Extra: overflow\nMulti: one\nMulti: two\nHyphen-Ated: Hyphen-Ated\n"
+            "\nThis is the body.\n",
+            Simple(
+                simple="foobar",
+                optional="present",
+                aliased="unknown",
+                multi=["one", "two"],
+                extra=[
+                    ("aliased", "extra"),
+                    ("extra", "overflow"),
+                    ("hyphen-ated", "Hyphen-Ated"),
+                ],
+                body="This is the body.\n",
+            ),
+        ),
+        (
+            MultiExtra,
+            "Foo: 42\nBar: true\nExtra: one\nExtra: two\nOther: stuff\n",
+            MultiExtra(
+                foo=42,
+                bar=True,
+                extra={"extra": ["one", "two"], "other": ["stuff"]},
+            ),
+        ),
+        (MultiExtra, "Bar: no\nFoo: 23\n", MultiExtra(foo=23, bar=False, extra={})),
+        (
+            CrissCross,
+            "ONE: apple\nTWO: banana\n",
+            CrissCross(one="banana", two="apple"),
+        ),
+    ],
+)
+def test_parse(cls: type, data: str, obj: Any) -> None:
+    assert parse(cls, data) == obj
+
+
+@pytest.mark.parametrize(
+    "data,exc_type,exc_str",
+    [
+        (
+            Simple,
+            "Simple: one\nSimple: two\n",
+            DuplicateFieldError,
+            "Header field 'simple' occurs more than once",
+        ),
+        (
+            Simple,
+            "Simple: foobar\nExtra: one\nExtra: two\n",
+            DuplicateFieldError,
+            "Header field 'extra' occurs more than once",
+        ),
+        (
+            Simple,
+            "",
+            TypeError,
+            "__init__() missing 1 required positional argument: 'simple'",
+        ),
+        (
+            MultiExtra,
+            "Foo: forty-two\n",
+            ValueError,
+            "invalid literal for int() with base 10: 'forty-two'",
+        ),
+        (MultiExtra, "Bar: maybe\n", ValueError, "invalid boolean: 'maybe'"),
+        (
+            MultiExtra,
+            "Some: field\n\nSome: body\n",
+            BodyNotAllowedError,
+            "Message body is present but not allowed",
+        ),
+        (
+            CrissCross,
+            "Unknown: field\n",
+            UnknownFieldError,
+            "Unknown header field 'unknown'",
+        ),
+    ],
+)
+def test_parse_error(
+    cls: type, data: str, exc_type: Type[Exception], exc_str: str
+) -> None:
+    with pytest.raises(exc_type) as excinfo:
+        parse(cls, data)
+    assert str(excinfo.value) == exc_str
